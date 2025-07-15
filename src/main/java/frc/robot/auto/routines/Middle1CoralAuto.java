cfeb089c1153ec7b7d516ceaaffd263dfb56bda6
hs150521 <hs150521@163.com>
2025-07-16 07:25:50 +0800

    [Feat] more untested auto

--- src/main/java/frc/robot/auto/routines/Middle1CoralAuto.java
+++ src/main/java/frc/robot/auto/routines/Middle1CoralAuto.java
@@ -1,4 +1,21 @@
 package frc.robot.auto.routines;
 
-public class Middle1CoralAuto {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.auto.AutoActions;
+import frc.robot.auto.AutoRoutine;
+import frc.robot.drivers.DestinationSupplier;
+
+public class Middle1CoralAuto extends AutoRoutine {
+    public Middle1CoralAuto() {
+        super("Middle1CoralAuto");
+    }
+
+    @Override
+    public Command getAutoCommand() {
+        return Commands.sequence(
+                AutoActions.reverseEE(),
+                AutoActions.setLevel(DestinationSupplier.elevatorSetpoint.L4),
+                AutoActions.autoScore('H'));
+    }
 }
--- src/main/java/frc/robot/auto/routines/RightStationIntakeAuto.java
+++ src/main/java/frc/robot/auto/routines/RightStationIntakeAuto.java
@@ -1,4 +1,47 @@
 package frc.robot.auto.routines;
 
-public class RightStationIntakeAuto {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.auto.AutoActions;
+import frc.robot.auto.AutoRoutine;
+import frc.robot.drivers.DestinationSupplier;
+
+public class RightStationIntakeAuto extends AutoRoutine {
+
+
+    public RightStationIntakeAuto() {
+        super("RightStationIntakeAuto");
+    }
+
+    @Override
+    public Command getAutoCommand() {
+        return Commands.sequence(
+                AutoActions.reverseEE(),
+                AutoActions.setLevel(DestinationSupplier.elevatorSetpoint.L4),
+                AutoActions.autoScore('F'),
+                Commands.parallel(
+                        AutoActions.toStation(false),
+                        AutoActions.intake()
+                ),
+                AutoActions.autoScore('D'),
+                Commands.parallel(
+                        AutoActions.toStation(false),
+                        AutoActions.intake()
+                ),
+                AutoActions.autoScore('C'),
+                Commands.parallel(
+                        AutoActions.toStation(false),
+                        AutoActions.intake()
+                ),
+                AutoActions.autoScore('E'),
+                Commands.parallel(
+                        AutoActions.toStation(false),
+                        AutoActions.intake()
+                ), AutoActions.autoScore('B'),
+                Commands.parallel(
+                        AutoActions.toStation(false),
+                        AutoActions.intake()
+                )
+        );
+    }
 }
